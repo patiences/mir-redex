7493454a089036e1a5c058d4482459f36faffd38
Patience Shyu <patienceshyu@gmail.com>
2017-04-04 21:19:50 -0700

    add match test

--- test-lang.rkt
+++ test-lang.rkt
@@ -191,11 +191,11 @@
 (define logical-or
   (term [(-> main () unit-type
              (mut _0 : unit-type)
-              (mut _1 : bool)
+             (mut _1 : bool)
              (bb bb0 [] (call _1
-                               foo [0 10 5]
-                               bb1))
-              (bb bb1 [(= _0 unit)] return))
+                              foo [0 10 5]
+                              bb1))
+             (bb bb1 [(= _0 unit)] return))
          (-> foo [(_1 : int) (_2 : int) (_3 : int)] bool
              (mut _0 : unit-type)
              (scope scope1
@@ -410,4 +410,81 @@
             (bb bb5 [] return))))
 
 (check-not-false (redex-match mir fn vec))
-            
+
+;; Match statements
+;; ======================================================
+;fn main() -> () {
+;    let mut _0: ();                      // return pointer
+;    scope 1 {
+;        let mut _1: i32;                 // "result" in scope 1 at <anon>:2:9: 2:19
+;        scope 2 {
+;            let _2: i32;                 // "transform" in scope 2 at <anon>:3:9: 3:18
+;            scope 3 {
+;                let _3: i32;             // "result" in scope 3 at <anon>:5:9: 5:15
+;            }
+;        }
+;    }
+;    let mut _4: i32;
+;    let mut _5: (i32, bool);
+;
+;    bb0: {
+;        StorageLive(_1);                 // scope 0 at <anon>:2:9: 2:19
+;        _1 = const 17i32;                // scope 0 at <anon>:2:22: 2:24
+;        StorageLive(_2);                 // scope 1 at <anon>:3:9: 3:18
+;        _2 = const 2i32;                 // scope 1 at <anon>:3:21: 3:22
+;        StorageLive(_3);                 // scope 2 at <anon>:5:9: 5:15
+;        switchInt(_2) -> [1i32: bb1, 2i32: bb2, otherwise: bb3]; // scope 2 at <anon>:6:9: 6:10
+;    }
+;
+;    bb1: {
+;        _3 = const 111i32;               // scope 2 at <anon>:6:14: 6:17
+;        goto -> bb4;                     // scope 2 at <anon>:5:18: 9:6
+;    }
+;
+;    bb2: {
+;        _3 = const 222i32;               // scope 2 at <anon>:7:14: 7:17
+;        goto -> bb4;                     // scope 2 at <anon>:5:18: 9:6
+;    }
+;
+;    bb3: {
+;        StorageLive(_4);                 // scope 2 at <anon>:8:14: 8:20
+;        _4 = _1;                         // scope 2 at <anon>:8:14: 8:20
+;        _5 = CheckedShr(_4, const 2i32); // scope 2 at <anon>:8:14: 8:25
+;        assert(!(_5.1: bool), "attempt to shift right with overflow") -> bb5; // scope 2 at <anon>:8:14: 8:25
+;    }
+;
+;    bb4: {
+;        _0 = ();                         // scope 3 at <anon>:1:11: 10:2
+;        StorageDead(_3);                 // scope 2 at <anon>:10:2: 10:2
+;        StorageDead(_2);                 // scope 1 at <anon>:10:2: 10:2
+;        StorageDead(_1);                 // scope 0 at <anon>:10:2: 10:2
+;        return;                          // scope 0 at <anon>:10:2: 10:2
+;    }
+;
+;    bb5: {
+;        _3 = (_5.0: i32);                // scope 2 at <anon>:8:14: 8:25
+;        StorageDead(_4);                 // scope 2 at <anon>:8:25: 8:25
+;        goto -> bb4;                     // scope 2 at <anon>:5:18: 9:6
+;    }
+;}
+
+(define match
+  (term (-> main () unit-type
+            (mut _0 : unit-type)
+            (scope scope1 (_1 : int)
+                   (scope scope2 (_2 : int)
+                          (scope scope3 (_3 : int))))
+            (mut _4 : int)
+            (mut _5 : (int bool))
+            (bb bb0
+                [(= _1 17)
+                 (= _2 2)]
+                (switchInt _2 (1 bb1) (2 bb2) (otherwise bb3)))
+            (bb bb1 [(= _3 111)] (goto bb4))
+            (bb bb2 [(= _3 222)] (goto bb4))
+            (bb bb3 [(= _4 (use _1)) (= _5 (>> (use _4) 2))]
+                (assert (! (use (· _5 1))) bb5 "attempt to shift right with overflow"))
+            (bb bb4 [(= _0 unit)] return)
+            (bb bb5 [(= _3 (use (· _5 0)))] (goto bb4)))))
+
+(check-not-false (redex-match mir fn match))
